45805743415272cda770a584a529a019e0cb99e9
sachink88 <71728876+sachink88@users.noreply.github.com>
2023-01-05 00:10:26 +0530

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-function dayNightMode(){
-                var mybody = document.body;
-                mybody.classList.toggle('mode2')
+function dayNightMode() {
+    var mybody = document.body;
+    mybody.classList.toggle('mode2')
 }
 
-function couponCode(){
+function couponCode() {
     document.getElementsByClassName('messageSender__coupon')[0].style.display = 'block';
-    document.getElementById('main').style.opacity='0.5'
+    document.getElementById('main').style.opacity = '0.5'
 }
 
 const closeCoupon = () => {
     document.getElementsByClassName('messageSender__coupon')[0].style.display = 'none';
-    document.getElementById('main').style.opacity='1'
+    document.getElementById('main').style.opacity = '1'
 }
+
