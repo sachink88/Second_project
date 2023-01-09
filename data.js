652de3343a5e62dc068ad0a3659386f55a053e3e
sachink88 <71728876+sachink88@users.noreply.github.com>
2023-01-10 01:34:59 +0530

    code

---
+++ data.js
@@ -0,0 +1,123 @@
+{
+    "data": [{
+            "id": "X999_Y999",
+            "from": {
+                "name": "Ricky Poiting",
+                "id": "X12"
+            },
+            "message": "Looking forward to ODI WC 2023!",
+            "actions": [{
+                    "name": "Comment",
+                    "link": "http://www.facebook.com/X999/posts/Y999"
+                },
+                {
+                    "name": "Like",
+                    "link": "http://www.facebook.com/X999/posts/Y999"
+                }
+            ],
+            "type": "status",
+            "created_time": "2023-01-07T21:27:44+0000",
+            "updated_time": "2023-01-07T21:27:44+0000"
+        },
+        {
+            "id": "X998_Y998",
+            "from": {
+                "name": "Vivian Richard",
+                "id": "X18"
+            },
+            "message": "Honoured 🙏⭐",
+            "actions": [{
+                    "name": "Comment",
+                    "link": "http://www.facebook.com/X998/posts/Y998"
+                },
+                {
+                    "name": "Like",
+                    "link": "http://www.facebook.com/X998/posts/Y998"
+                }
+            ],
+            "type": "status",
+            "created_time": "2023-01-07T21:27:44+0000",
+            "updated_time": "2023-01-07T21:27:44+0000"
+        },
+        {
+            "id": "X997_Y997",
+            "from": {
+                "name": "Ben Stokes",
+                "id": "X21"
+            },
+            "message": "I’m donating my match fees from this Test series to the Pakistan Flood appeal ❤️🇵🇰",
+            "actions": [{
+                    "name": "Comment",
+                    "link": "http://www.facebook.com/X997/posts/Y997"
+                },
+                {
+                    "name": "Like",
+                    "link": "http://www.facebook.com/X997/posts/Y997"
+                }
+            ],
+            "type": "status",
+            "created_time": "2023-01-07T21:27:44+0000",
+            "updated_time": "2023-01-07T21:27:44+0000"
+        },
+        {
+            "id": "X996_Y996",
+            "from": {
+                "name": "Brett Lee",
+                "id": "X28"
+            },
+            "message": "Hello Sydney! Good to be 🏠",
+            "actions": [{
+                    "name": "Comment",
+                    "link": "http://www.facebook.com/X996/posts/Y996"
+                },
+                {
+                    "name": "Like",
+                    "link": "http://www.facebook.com/X996/posts/Y996"
+                }
+            ],
+            "type": "status",
+            "created_time": "2023-01-07T21:27:44+0000",
+            "updated_time": "2023-01-07T21:27:44+0000"
+        },
+        {
+            "id": "X995_Y995",
+            "from": {
+                "name": "AB de Villiers",
+                "id": "X26"
+            },
+            "message": "Incredible how catches change momentum in Cricket",
+            "actions": [{
+                    "name": "Comment",
+                    "link": "http://www.facebook.com/X995/posts/Y995"
+                },
+                {
+                    "name": "Like",
+                    "link": "http://www.facebook.com/X995/posts/Y995"
+                }
+            ],
+            "type": "status",
+            "created_time": "2023-01-07T21:27:44+0000",
+            "updated_time": "2023-01-07T21:27:44+0000"
+        },
+        {
+            "id": "X994_Y994",
+            "from": {
+                "name": "Virat Kohli",
+                "id": "X38"
+            },
+            "message": "Get well soon @RishabhPant17 Praying for your recovery. 🙏🏻",
+            "actions": [{
+                    "name": "Comment",
+                    "link": "http://www.facebook.com/X994/posts/Y994"
+                },
+                {
+                    "name": "Like",
+                    "link": "http://www.facebook.com/X994/posts/Y994"
+                }
+            ],
+            "type": "status",
+            "created_time": "2023-01-07T21:27:44+0000",
+            "updated_time": "2023-01-07T21:27:44+0000"
+        }
+    ]
+}
